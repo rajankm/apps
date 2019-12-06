86c50f5d0007a222ca015b67e1ee75f21f73ae2f
mishrarajan <mishra.k.rajan@gmail.com>
2019-12-06 19:32:45 +0530

    Initial-version

---
+++ auth-management/src/main/java/com/auth/AuthManagementApplication.java
@@ -0,0 +1,13 @@
+package com.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthManagementApplication.class, args);
+	}
+
+}
